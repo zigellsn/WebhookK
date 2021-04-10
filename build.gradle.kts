1969ca5a4b7131aa0e17a874ef22ac6b930782ca
Simon Zigelli <zigellsn@gmail.com>
2021-04-10 11:50:10 +0200

    Enhance build process

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 Simon Zigelli
+ * Copyright 2019-2021 Simon Zigelli
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 
 import org.gradle.jvm.tasks.Jar
 
-val kotlinVersion by extra ("1.4.32")
-val ktorVersion by extra ("1.5.3")
-val coroutinesVersion by extra ("1.4.3")
+val kotlinVersion by extra("1.4.32")
+val ktorVersion by extra("1.5.3")
+val coroutinesVersion by extra("1.4.3")
 
 plugins {
     kotlin("jvm") version "1.4.32"
@@ -28,27 +28,22 @@ plugins {
     `java-library`
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.github.zigellsn"
-            artifactId = "webhookk"
-            version = "1.1.0-beta01"
-
-            from(components["java"])
-        }
-    }
-}
+group = "com.github.zigellsn"
+version = "1.1.0-beta02"
 
 repositories {
     mavenCentral()
-    maven(url="https://dl.bintray.com/kotlin/dokka")
-    maven(url="https://dl.bintray.com/kotlin/kotlinx")
+    maven(url = "https://dl.bintray.com/kotlin/dokka")
+    maven(url = "https://dl.bintray.com/kotlin/kotlinx")
 }
 
 kotlin {
     explicitApi()
     explicitApiWarning()
+}
+
+java {
+    withSourcesJar()
 }
 
 dependencies {
@@ -82,4 +77,40 @@ val dokkaJar by tasks.creating(Jar::class) {
     description = "Assembles Kotlin docs with Dokka"
     archiveClassifier.set("javadoc")
     from(tasks.dokkaHtml)
+}
+
+tasks.build.configure {
+    dependsOn(dokkaJar)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifact(dokkaJar)
+            pom {
+                name.set("WebhookK")
+                description.set("A Kotlin webhook provider")
+                url.set("https://github.com/zigellsn/WebhookK/")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("zigellsn")
+                        name.set("Simon Zigelli")
+                        email.set("zigellsn@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/zigellsn/WebhookK.git")
+                    developerConnection.set("scm:git:ssh://github.com/zigellsn/WebhookK.git")
+                    url.set("https://github.com/zigellsn/WebhookK/")
+                }
+            }
+            from(components["java"])
+        }
+    }
 }
